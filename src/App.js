979d76db1b44768f9758b76794727b294ac6436d
Abraham Tsang <abraham.tsang@gmail.com>
2020-12-28 20:41:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import React from 'react';
+
+import {ScatterChart, XAxis, YAxis, ZAxis, Scatter, CartesianGrid, Tooltip, Legend} from 'recharts';
+
 import logo from './logo.svg';
 import './App.css';
 
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +26,57 @@ function App() {
     </div>
   );
 }
+*/
+
+  const africadata = [
+    {x: 50, y: 300, z: 200},
+    {x: 20, y: 150, z: 150},
+    {x: 40, y: 450, z: 320},
+    {x: 30, y: 650, z: 330},
+    {x: 10, y: 350, z: 130},
+  ];
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.allowDrop = this.allowDrop.bind(this);
+    this.drag = this.drag.bind(this);
+    this.drop = this.drop.bind(this);
+  }
+
+  allowDrop = (ev) => {
+    ev.preventDefault();
+  }
+
+  drag = (ev) => {
+    ev.dataTransfer.setData("Text", ev.target.id);
+  }
+
+  drop = (ev) => {
+    var data = ev.dataTransfer.getData("Text");
+    console.log("abc");
+    ev.preventDefault();
+  }
+
+  render(){
+    return(
+      <div>
+        <div onDrop={(event => this.drop(event))} onDragOver={(event => this.allowDrop(event))}>
+	  <ScatterChart width={730} height={250} margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+            <XAxis type="number" dataKey="x" />
+            <YAxis type="number" dataKey="y" />
+            <ZAxis type="number" dataKey="z" range={[60, 400]} />
+            <CartesianGrid strokeDasharray="3 3"/>
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Legend />
+            <Scatter name="data" data={africadata} fill="#8884d8"/>
+          </ScatterChart>
+        </div>
+        <p draggable="true" onDragStart={(event => this.drag(event))}>Testing</p>
+      </div>
+    );
+  }
+}
+
 
 export default App;
